2ab7e23673c2937ae8b6f57b409f8d2f440c0e31
marcioscardua <marciooscardua@gmail.com>
2021-06-16 02:53:45 -0400

    Passport configurado e comentado para ambiente de desenvolvimento

---
+++ src/passport.js
@@ -0,0 +1,27 @@
+const knex = require('./database')
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+
+const {secret} = require('../.env')
+const {Strategy, ExtractJwt} = passportJwt
+
+const params = {
+    secretOrKey : secret,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+}
+
+const strategy = new Strategy(params, (payload, done) =>{
+
+    knex('users')
+        .where({id:payload.id})
+        .first()
+        .then(user => done(null, user ? {...payload} : false))
+        .catch(err => done(err, false))
+})
+
+passport.use(strategy)
+
+module.exports = {
+
+    authenticate: () => passport.authenticate('jwt', {session:false})
+}
--- src/routes.js
+++ src/routes.js
@@ -8,53 +8,77 @@ const MaterialController = require('./controllers/MaterialController')
 const MachineController = require('./controllers/MachineController')
 const OrderController = require('./controllers/OrderController')
 const Validate = require('./controllers/ValidateController')
+const auth = require('./passport')
+
 
 //Global ---
 routes.post('/signin', Validate.signin)
 routes.post('/signup', UserController.create)
 routes.post('/validateToken', Validate.validateToken)
 
+
 //Users Routes
+//routes.all('/users', auth.authenticate())
 routes.get('/users', UserController.list )
 routes.post('/users', UserController.create)
+
+//routes.all('/users/:id', auth.authenticate())
 routes.put('/users/:id', UserController.update)
 routes.delete('/users/:id', UserController.delete)
 
 //Product Routes
+//routes.all('/products', auth.authenticate())
 routes.get('/products', ProductController.list)
 routes.post('/products', ProductController.create)
-routes.put('/products/:id', ProductController.update)
 routes.put('/products', ProductController.updateQuery)
+
+//routes.all('/products/:id', auth.authenticate())
+routes.put('/products/:id', ProductController.update)
 routes.delete('/products/:id', ProductController.delete)
 
 //Colors Routes
+//routes.all('/colors', auth.authenticate())
 routes.get('/colors',ColorController.list )
 routes.post('/colors', ColorController.create)
+
+//routes.all('/colors/:id', auth.authenticate())
 routes.put('/colors/:id', ColorController.update)
 routes.delete('/colors/:id', ColorController.delete)
 
 //Fonts Routes
+//routes.all('/fonts', auth.authenticate())
 routes.get('/fonts',FontController.list )
 routes.post('/fonts', FontController.create)
+
+//routes.all('/fonts/:id', auth.authenticate())
 routes.put('/fonts/:id', FontController.update)
 routes.delete('/fonts/:id', FontController.delete)
 
 
 //Materials Routes
+//routes.all('/materials', auth.authenticate())
 routes.get('/materials',MaterialController.list )
 routes.post('/materials', MaterialController.create)
+
+//routes.all('/materials/:id', auth.authenticate())
 routes.put('/materials/:id', MaterialController.update)
 routes.delete('/materials/:id', MaterialController.delete)
 
 //Machines Routes
+//routes.all('/machines/', auth.authenticate())
 routes.get('/machines',MachineController.list )
 routes.post('/machines', MachineController.create)
+
+//routes.all('/machines/:id', auth.authenticate())
 routes.put('/machines/:id', MachineController.update)
 routes.delete('/machines/:id', MachineController.delete)
 
 //Order Routes
+//routes.all('/orders', auth.authenticate())
 routes.get('/orders',OrderController.list )
 routes.post('/orders', OrderController.create)
+
+//routes.all('/orders/:id', auth.authenticate())
 routes.put('/orders/:id', OrderController.update)
 routes.delete('/orders/:id', OrderController.delete)
 
